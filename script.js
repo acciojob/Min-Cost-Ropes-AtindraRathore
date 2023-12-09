ddf2c5b0a1bf6123a0cc941ab7a2c6749256efcc
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-10 01:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let cost = 0;
+  while(arr.length>1){
+	  let a = arr.pop();
+	  let b = arr.pop();
+       let sum = a+b;
+	  cost += sum;
+	  arr.push(sum);
+  }
+	return cost;
 }
 
 module.exports=mincost;
