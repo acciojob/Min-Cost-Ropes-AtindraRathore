7cba9c4cf6eba1f31893551bd1bb5bbe6dddbaf1
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-12 01:47:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function mincost(arr)
 { 
+	let pq = new PriorityQueue();
 //write your code here
 // return the min cost
+	for(let i=0;i<arr.length;i++){
+		pq.add(arr[i]);
+	}
 	let cost = 0;
 	arr.sort();
   while(arr.length>1){
-	  let a = arr.shift();
-	  let b = arr.shift();
+	  let a = pq.pop();
+	  let b = pq.pop();
        let sum = a+b;
        console.log(arr);
 	  cost = cost+ sum;
-	  arr.push(sum);
-	  console.log(arr);
+	  pq.add(sum);
+	 
   }
 	return cost;
 };
